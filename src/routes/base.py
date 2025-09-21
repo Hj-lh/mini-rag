68653ef0048ed7b2b36ece56f698c0abe8f7f250
Hj-lh <443102336@Student.ksu.edu.sa>
2025-09-21 20:38:25 +0300

    Basic

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, APIRouter
 
 base_router = APIRouter(
-    
+
 )
 
 @base_router.get("/welcome")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
